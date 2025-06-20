d4316006375cfdc9957c617d230847737958128d
Guilherme <enshihhu.20231@poli.ufrj.br>
2025-06-20 15:07:36 -0300

    Update 00_Deleção_Dados.sql

--- 00_Deleção_Dados.sql
+++ 00_Deleção_Dados.sql
@@ -5,12 +5,10 @@ DELETE FROM dbo.UnidadeMedica_Vacinador;
 DELETE FROM dbo.Vacinacao;
 DELETE FROM dbo.Endereco;
 DELETE FROM dbo.Nacionalidade;
-
 DELETE FROM dbo.Dose;
 DELETE FROM dbo.Ampola;
 DELETE FROM dbo.Lote;
 DELETE FROM dbo.TipoVacina;
-
 DELETE FROM dbo.Paciente;
 DELETE FROM dbo.Vacinador;
 DELETE FROM dbo.Fabricante;
@@ -18,7 +16,6 @@ DELETE FROM dbo.UnidadeMedica;
 DELETE FROM dbo.PessoaJuridica;
 DELETE FROM dbo.PessoaFisica;
 DELETE FROM dbo.Pessoa;
-
 DELETE FROM dbo.Complemento;
 DELETE FROM dbo.TipoComplemento;
 DELETE FROM dbo.Logradouro;
