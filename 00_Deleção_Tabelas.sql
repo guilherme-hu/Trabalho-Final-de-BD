38a6e335abdbc15d634da045fc4a3982b0bbf005
Guilherme <enshihhu.20231@poli.ufrj.br>
2025-06-20 00:24:22 -0300

    Create 00_Deleção_Tabelas.sql

---
+++ 00_Deleção_Tabelas.sql
@@ -0,0 +1,31 @@
+USE BD_vacinacao;
+GO
+
+DROP TABLE IF EXISTS dbo.UnidadeMedica_Vacinador;
+DROP TABLE IF EXISTS dbo.Vacinacao;
+DROP TABLE IF EXISTS dbo.Endereco;
+DROP TABLE IF EXISTS dbo.Nacionalidade;
+
+DROP TABLE IF EXISTS dbo.Dose;
+DROP TABLE IF EXISTS dbo.Ampola;
+DROP TABLE IF EXISTS dbo.Lote;
+DROP TABLE IF EXISTS dbo.TipoVacina;
+
+DROP TABLE IF EXISTS dbo.Paciente;
+DROP TABLE IF EXISTS dbo.Vacinador;
+DROP TABLE IF EXISTS dbo.Fabricante;
+DROP TABLE IF EXISTS dbo.UnidadeMedica;
+DROP TABLE IF EXISTS dbo.PessoaJuridica;
+DROP TABLE IF EXISTS dbo.PessoaFisica;
+DROP TABLE IF EXISTS dbo.Pessoa;
+
+DROP TABLE IF EXISTS dbo.Complemento;
+DROP TABLE IF EXISTS dbo.TipoComplemento;
+DROP TABLE IF EXISTS dbo.Logradouro;
+DROP TABLE IF EXISTS dbo.TipoLogradouro;
+DROP TABLE IF EXISTS dbo.TipoEndereco;
+DROP TABLE IF EXISTS dbo.Bairro;
+DROP TABLE IF EXISTS dbo.Municipio;
+DROP TABLE IF EXISTS dbo.UF;
+DROP TABLE IF EXISTS dbo.Pais;
+GO
