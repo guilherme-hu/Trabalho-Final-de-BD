7d45238f9b2686c7e03faf5d0186aaa16d76558e
Guilherme <enshihhu.20231@poli.ufrj.br>
2025-06-20 21:46:00 -0300

    Update 00_Seleção_Tudo.sql

--- 00_Seleção_Tudo.sql
+++ 00_Seleção_Tudo.sql
@@ -1,28 +1,28 @@
 USE BD_vacinacao;
 GO
 
-SELECT * FROM dbo.Pais;
-SELECT * FROM dbo.UF;
-SELECT * FROM dbo.Municipio;
+SELECT * FROM dbo.Ampola;
 SELECT * FROM dbo.Bairro;
-SELECT * FROM dbo.TipoLogradouro;
+SELECT * FROM dbo.Complemento;
+SELECT * FROM dbo.Dose;
+SELECT * FROM dbo.Endereco;
+SELECT * FROM dbo.Fabricante;
 SELECT * FROM dbo.Logradouro;
+SELECT * FROM dbo.Lote;
+SELECT * FROM dbo.Municipio;
+SELECT * FROM dbo.Nacionalidade;
+SELECT * FROM dbo.Paciente;
+SELECT * FROM dbo.Pais;
 SELECT * FROM dbo.Pessoa;
 SELECT * FROM dbo.PessoaFisica;
 SELECT * FROM dbo.PessoaJuridica;
-SELECT * FROM dbo.Fabricante;
-SELECT * FROM dbo.UnidadeMedica;
-SELECT * FROM dbo.Paciente;
-SELECT * FROM dbo.Vacinador;
-SELECT * FROM dbo.Nacionalidade;
 SELECT * FROM dbo.TipoComplemento;
-SELECT * FROM dbo.Complemento;
 SELECT * FROM dbo.TipoEndereco;
-SELECT * FROM dbo.Endereco;
+SELECT * FROM dbo.TipoLogradouro;
 SELECT * FROM dbo.TipoVacina;
-SELECT * FROM dbo.Lote;
-SELECT * FROM dbo.Ampola;
-SELECT * FROM dbo.Dose;
-SELECT * FROM dbo.Vacinacao;
+SELECT * FROM dbo.UF;
+SELECT * FROM dbo.UnidadeMedica;
 SELECT * FROM dbo.UnidadeMedica_Vacinador;
+SELECT * FROM dbo.Vacinacao;
+SELECT * FROM dbo.Vacinador;
 GO
